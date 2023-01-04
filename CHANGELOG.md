db76155fee48aaee9a4510dc82d8bae74791f9a5
e-magon <emagon@emagon.xyz>
2023-01-04 10:58:43 +0100

    Init commit

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+## [2.1.0]
+
+
+### Features
+
+* added filters string check and unit tests
+
+### [2.0.1]
+
+
+### Bug Fixes
+
+* offset calculation
+
+## 2.0.0 (2019-11-29)
+
+
+### ⚠ BREAKING CHANGES
+
+* symfony 5
+
+### Features
+
+* symfony 5 upgrade
+
+### 1.0.1 (2019-09-10)
+
+## 1.0.0 (2019-09-10)
---
+++ src/RequestParams.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace K3;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
+
+class RequestParams
+{
+    public static function fromRequest(Request $request): AttributeBag
+    {
+        $requestParams = new AttributeBag();
+        $filters = $request->get('filters', '{}');
+        if (!is_string($filters)) {
+            throw new \RuntimeException('Filters must be a string');
+        }
+        $requestParams->set('filters', json_decode($filters, true, 512, JSON_THROW_ON_ERROR) ?? []);
+
+        $sortBy = $request->get('sortBy');
+        $requestParams->set(
+            'sortBy',
+            $sortBy === null ? [] : [$sortBy => $request->get('descending') ? 'DESC' : 'ASC']
+        );
+
+        $rowsPerPage = (int)$request->get('rowsPerPage');
+        $requestParams->set('limit', $rowsPerPage > 0 ? $rowsPerPage : null);
+
+        $page = (int)$request->get('page');
+        $requestParams->set('offset', ($page > 0 ? $page - 1 : 0) * ($rowsPerPage ?: 0));
+
+        return $requestParams;
+    }
+}
